a40ab465a5d8f3931aa170d676ba72f0ce191e18
Ziabko <146750145+Ziabko@users.noreply.github.com>
2024-08-24 19:42:53 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,43 +1,16 @@
+package de.telran;
+
 public class Main {
     public static void main(String[] args) {
 
-        //Создайте строку через new - I study Basic Java!
-        //Распечатать пред-последний символ строки. Используем метод String.charAt().
-        String str1 = new String("I study Basic Java!");
-        System.out.println(str1.charAt(17));
+        Module1Lecture1Person person1 = new Module1Lecture1Person();
+        person1.move();
+        person1.talk();
 
 
-        //Проверить, содержит ли ваша строка подстроку “Java”.
-        //Используем метод String.contains().
-        boolean containsJava = str1.contains("Java");
-        System.out.println();
-        if (containsJava) {
-            System.out.println("Строка содержит 'Java'.");
-        } else {
-            System.out.println("Строка не содержит 'Java'.");
-        }
+        Module1Lecture1Person person2 = new Module1Lecture1Person("John", 30);
+        person2.move();
+        person2.talk();
 
-
-        //Вырезать строку Java c помощью метода String.substring().
-        System.out.println(str1.substring(14, 18));
-
-        // Заменить все символы “а” на “о”.
-        String replacedString = str1.replace('a', 'o');
-        System.out.println(replacedString);
-
-        //Преобразуйте строку к верхнему регистру.
-        String upperCaseStr1 = str1.toUpperCase();
-        System.out.println(upperCaseStr1);
-
-        //Преобразуйте строку к нижнему регистру.
-        String lowerCaseStr1 = str1.toLowerCase();
-        System.out.println(lowerCaseStr1);
-
-
-
-
-
-
-
-    }
+   }
 }
